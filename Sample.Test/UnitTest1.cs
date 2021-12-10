bf625c0ae53eba863fc57ea871551bbed931f36e
allenbenz <allenbenz@users.noreply.github.com>
2021-12-10 11:07:02 -0800

    initial

---
+++ Sample.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Sample.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
